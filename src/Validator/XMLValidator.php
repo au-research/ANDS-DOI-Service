ecff5f93492315e014df0557171a1e76036babd3
Liz Woods <u3485947@devl.ands.org.au>
2016-08-23 13:25:22 +1000

    added xml version extraction and test

--- src/Validator/XMLValidator.php
+++ src/Validator/XMLValidator.php
@@ -7,12 +7,70 @@ namespace ANDS\DOI\Validator;
 class XMLValidator
 {
     /**
+     * determines datacite xml schema version
+     *
+     * @param $xml
+     * @return string
+     */
+
+    public function getSchemaVersion($xml){
+
+        $doiXML = new \DOMDocument();
+        $doiXML->loadXML($xml);
+
+        $resources = $doiXML->getElementsByTagName('resource');
+        $theSchema = 'unknown';
+        if($resources->length>0)
+        {
+            if(isset($resources->item(0)->attributes->item(0)->name))
+            {
+                $theSchema  = self::getXmlSchema($resources->item(0)->attributes->item(0)->nodeValue);
+            }
+        }
+        return $theSchema;
+
+    }
+
+
+    /**
+     * Helper function to determine version number from xml resource string
+     *
+     * @param $theSchemaLocation
+     * @return string
+     */
+
+    public static function getXmlSchema($theSchemaLocation)
+    {
+        if(str_replace("kernel-2.0","",$theSchemaLocation)!=$theSchemaLocation)
+        {
+            return "2.0";
+        }
+        elseif(str_replace("kernel-2.1","",$theSchemaLocation)!=$theSchemaLocation)
+        {
+            return "2.1";
+        }
+        elseif(str_replace("kernel-2.2","",$theSchemaLocation)!=$theSchemaLocation)
+        {
+            return "2.2";
+        }
+        elseif(str_replace("kernel-3","",$theSchemaLocation)!=$theSchemaLocation)
+        {
+            return "3";
+        }
+        else
+        {
+            return "unknown";
+        }
+    }
+
+    /**
      * Replaces the DOI Identifier value in the provided XML
      *
      * @param $doiValue
      * @param $xml
      * @return string
      */
+
     public static function replaceDOIValue($doiValue, $xml)
     {
         $doiXML = new \DOMDocument();
