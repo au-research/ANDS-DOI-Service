f87c1d106fa2c0a3742c642efca6475d571f2314
Elizabeth Margaret Woods <liz.woods@ands.org.au>
2017-05-05 08:20:39 +1000

    amended so that a manualy minted doi will use the doi value constructed in the manual mint tool

--- src/DOIServiceProvider.php
+++ src/DOIServiceProvider.php
@@ -126,7 +126,7 @@ class DOIServiceProvider
      * @param $xml
      * @return bool
      */
-    public function mint($url, $xml)
+    public function mint($url, $xml, $manual)
     {
 
         // @todo event handler, message
@@ -145,8 +145,12 @@ class DOIServiceProvider
             return false;
         }
 
-        // construct DOI
-        $doiValue = $this->getNewDOI();
+        // construct DOI 
+        if($manual===true) { 
+            $doiValue = XMLValidator::getDOIValue($xml); 
+        }else{ 
+            $doiValue = $this->getNewDOI(); 
+        }
         $this->setResponse('doi', $doiValue);
 
         // validation on the DOIValue
