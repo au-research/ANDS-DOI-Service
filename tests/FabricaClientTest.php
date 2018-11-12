2bae2c187cb1ae663e5eba89d30fefcd92c65248
Liz Woods <liz.woods@ands.org.au>
2018-11-12 12:48:16 +1100

    fix to env variable

--- tests/FabricaClientTest.php
+++ tests/FabricaClientTest.php
@@ -202,8 +202,8 @@ class FabricaClientTest extends PHPUnit_Framework_TestCase
     /** @test  **/
     public function it_should_get_list_of_client_dois()
     {
-        $pro_doi_client = getenv("GET_DOI_DATA_CENTRE");
-        $trustedCient = $this->fabricaClient->getClientByDataCiteSymbol( $pro_doi_client);
+       // $pro_doi_client = getenv("GET_DOI_DATA_CENTRE");
+        $trustedCient = $this->fabricaClient->getClientByDataCiteSymbol("ANDS.CENTER14");
         $dois = $trustedCient->getDOIs();
         $this->assertTrue(count($dois)> 0);
     }
